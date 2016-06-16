35c6caed121c8ffaada5dc30cada489a566a4fb6
liushengnan <liushengnan1@le.com>
2016-06-16 14:41:01 +0800

    all

---
+++ CommonFramework/CommonFramework.m
@@ -0,0 +1,27 @@
+//
+//  CommonFramework.m
+//  CommonFramework
+//
+//  Created by shengnan.liu on 16/6/16.
+//  Copyright © 2016年 shengnan.liu. All rights reserved.
+//
+
+#import "CommonFramework.h"
+
+@implementation CommonFramework
+
+- (void)fangfaOne
+{
+    NSLog(@"fangfaOne");
+}
+
+-(void)fangfaTwo
+{
+    NSLog(@"fangfaTwo");
+}
+- (void)fangfaThree
+{
+    NSLog(@"fangfaThree");
+}
+
+@end
