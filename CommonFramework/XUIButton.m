bb460fa21dcb07a948160f90fdd7dcd0fc930388
liushengnan <liushengnan1@le.com>
2016-06-16 15:35:17 +0800

    add file

---
+++ CommonFramework/XUIButton.m
@@ -0,0 +1,18 @@
+//
+//  XUIButton.m
+//  CommonFramework
+//
+//  Created by shengnan.liu on 16/6/16.
+//  Copyright © 2016年 shengnan.liu. All rights reserved.
+//
+
+#import "XUIButton.h"
+
+@implementation XUIButton
+
+- (void)changeBGColor:(UIColor *)color
+{
+    self.backgroundColor = color;
+}
+
+@end
